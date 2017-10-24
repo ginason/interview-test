16df0f521ccabf3d5e8b06bceea0e2e6d760352d
Omer Dobrescu <omerdobrescu@github.com>
2017-10-24 17:18:55 -0400

    :lipstick: add style to shop page

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -4,17 +4,13 @@ import React from "react";
 const Header = ({ children, goBack }) => {
   return (
     <header>
-      <div className="left-content">
-        <button onClick={goBack} className="back-button">
-          <img alt="Back" src={`/icon/header/back-white.svg`} />
-        </button>
-      </div>
-      <div className="middle-content">
-        <h1>
-          {children}
-        </h1>
-      </div>
-      <div className="right-content"></div>
+      <button onClick={goBack} className="back-button">
+        <img alt="Back" src={`/icon/header/back-white.svg`} />
+      </button>
+      <h1>
+        {children}
+      </h1>
+      <div className="right-content" />
     </header>
   );
 };
--- imports/ui/components/Product.jsx
+++ imports/ui/components/Product.jsx
@@ -12,21 +12,39 @@ class Product extends PureComponent {
   render() {
     const {
       name = "Product",
+      image,
       brand,
       color,
       description,
-      id,
-      image,
       price,
-      quantity,
       size
     } = this.props;
+
+    const info = [
+      { label: "Brand", value: brand },
+      { label: "Name", value: name },
+      { label: "Description", value: description },
+      { label: "Color", value: color },
+      { label: "Size", value: size },
+      { label: "Price", value: price }
+    ];
+
     return (
       <div className="product">
-        <div className="image-container">
-          <img alt={name} src={image} />
-        </div>
-        <div className="product-details-container">
+        <img alt={name} src={image} />
+        <div className="details">
+          <div className="info">
+            {info.map(({ label, value }) =>
+              <div className="info-row" key={`${label}${value}${name}`}>
+                <div className="label">
+                  {label}:
+                </div>
+                <div className="value">
+                  {value}
+                </div>
+              </div>
+            )}
+          </div>
           <Button onClick={this.handleBuyProduct}>
             Buy {name}
           </Button>
